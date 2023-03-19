fb9fbd61f73de22c00912ee6012baf9591994c58
Anshu Sinha <anshujuly2@gmail.com>
2023-03-19 11:40:52 +0530

    Add creating, inserting, selecting and deleting elements using dom

--- script.js
+++ script.js
@@ -31,3 +31,52 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
+// 186. selecting, creating and deleting elements
+
+// selecting elements
+console.log(document.documentElement); // selects entire html document
+console.log(document.head); // selects the head
+console.log(document.body); // selects the body
+
+// querySelector
+const header = document.querySelector(".header");
+// querySelectorAll
+const allSections = document.querySelectorAll(".section");
+console.log(allSections);
+
+// getElementById
+console.log(document.getElementById("section--1"));
+// getElementsByTagName
+const allButtons = document.getElementsByTagName("button");
+console.log(allButtons);
+
+// getElementsByClassName
+console.log(document.getElementsByClassName("btn"));
+
+// creating and inserting elements
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+
+message.innerHTML =
+  "Welcome to Galactic Credits Union! Just like every planet has its own unique flavor, we use cookies to enhance your browsing experience and make banking with us even more convenient. By using our app, you consent to our use of cookies. To learn more, please read our Cookie Policy. <button class='btn btn--close-cookie'>Got it!</button>";
+
+// header.prepend(message);
+header.append(message);
+
+// header.before(message);
+// header.after(message);
+
+// delete elements
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
